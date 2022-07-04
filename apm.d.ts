27d75062efb6e4170c24c746a348041379a44d94
Comandeer <vepomoc@gmail.com>
2022-07-04 23:59:29 +0200

    chore(apm.d.ts): add some basic typings

--- apm.d.ts
+++ apm.d.ts
@@ -1,1 +1,9 @@
-export {};
+interface DetectionResult {
+	name: 'npm' | 'pnpm' | 'yarn';
+	method: 'packageManager' | 'lock-file' | 'fallback';
+}
+
+declare function detectPackageManager( path: string ): Promise<DetectionResult>;
+
+export { DetectionResult };
+export { detectPackageManager };
